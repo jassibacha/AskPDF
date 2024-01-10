1027ee21c6ef3457af4dcd88b2ca94b69450fc01
Jassi Bacha <klikster@gmail.com>
2024-01-09 19:34:53 -0800

    Navbar built, Kinde integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/NavBar'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={cn(
         'min-h-screen font-sans antialiased grainy', inter.className
       )}>
+        <NavBar />
         {children}
       </body>
     </html>
