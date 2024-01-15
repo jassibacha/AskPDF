195073e82dcb90445e7e08bf43fed745a64f0e02
Jassi Bacha <klikster@gmail.com>
2024-01-15 13:32:03 -0800

    Fix user message icon bg

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -16,7 +16,7 @@ function Message({ message, isNextMessageSamePerson}: MessageProps) {
             "justify-end": message.isUserMessage,
         })}>
             <div className={cn("relative flex h-6 w-6 aspect-square items-center justify-center", {
-                "order-2 bg-blue rounded-sm": message.isUserMessage,
+                "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
                 "order-1 bg-zinc-800 rounded-sm": !message.isUserMessage,
                 invisible: isNextMessageSamePerson
             })}>
